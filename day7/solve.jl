83ff1e041e576d1ca1c425d49dd7d0d96c9a53a7
Neal Grantham <neal@nsgrantham.com>
2021-12-07 08:22:15 +0200

    Change start index

--- day7/solve.jl
+++ day7/solve.jl
@@ -1,13 +1,11 @@
 crabs = parse.(Int, split(readline("day7/crabs.txt"), ','))
 
 move_crabs(crabs, to) = sum(abs.(crabs .- to))
-
 p1 = minimum(move_crabs(crabs, i) for i in 0:maximum(crabs))
 
 @show p1
 
-move_crabs2(crabs, to) = sum(sum(0:abs(crab - to)) for crab in crabs)
-
+move_crabs2(crabs, to) = sum(sum(1:abs(crab - to)) for crab in crabs)
 p2 = minimum(move_crabs2(crabs, i) for i in 0:maximum(crabs))
 
 @show p2
