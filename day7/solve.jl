fad5a096a1166b7ba0510facd466dbb62976986e
Neal Grantham <neal@nsgrantham.com>
2021-12-07 08:18:40 +0200

    Shorten code

--- day7/solve.jl
+++ day7/solve.jl
@@ -1,17 +1,13 @@
 crabs = parse.(Int, split(readline("day7/crabs.txt"), ','))
 
-function move_crabs(crabs, to)
-    sum(abs.(crabs .- to))
-end
+move_crabs(crabs, to) = sum(abs.(crabs .- to))
 
-p1 = min([move_crabs(crabs, i) for i in 0:max(crabs...)]...)
+p1 = minimum(move_crabs(crabs, i) for i in 0:maximum(crabs))
 
 @show p1
 
-function move_crabs2(crabs, to)
-    sum(sum(0:abs(crab - to)) for crab in crabs)
-end
+move_crabs2(crabs, to) = sum(sum(0:abs(crab - to)) for crab in crabs)
 
-p2 = min([move_crabs2(crabs, i) for i in 0:max(crabs...)]...)
+p2 = minimum(move_crabs2(crabs, i) for i in 0:maximum(crabs))
 
 @show p2
