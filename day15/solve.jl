26a1bd8a608e2ab83349322610388951c8bcdf15
Neal Grantham <neal@nsgrantham.com>
2021-12-17 16:39:08 -0500

    Update for Julia 1.7

--- day15/solve.jl
+++ day15/solve.jl
@@ -1,46 +1,55 @@
 
-function find_lowest_total_risk(risks)
-    cave = CartesianIndices(risks)
+function find_low_risk_path(chitons)
+    cavern = CartesianIndices(chitons)
     moves = CartesianIndex.(((1, 0), (-1, 0), (0, 1), (0, -1)))
-    start, finish = extrema(cave)
-    total_risk = fill(typemax(Int), size(risks))
-    total_risk[start] = 0
-    heap = [(start, 0)]
-    while !isempty(heap)
-        position, risk = popat!(heap, last(findmin([last(h) for h in heap])))
-        position == finish && return risk
-        total_risk[position] < risk && continue
-        total_risk[position] = risk
-        for neighbor in [position + move for move in moves if position + move in cave]
-            neighbor_risk = risk + risks[neighbor]
-            if neighbor_risk < total_risk[neighbor]
-                total_risk[neighbor] = neighbor_risk
-                push!(heap, (neighbor, neighbor_risk))
+    start, finish = extrema(cavern)
+    prev_position = Dict{CartesianIndex{2}, CartesianIndex{2}}()
+    tentative_risk = fill(typemax(Int), size(chitons))
+    tentative_risk[start] = 0
+    queue = [(start, 0)]
+    while !isempty(queue)
+        position, risk = popat!(queue, last(findmin(last, queue)))
+        if position == finish
+            path = [finish]
+            while first(path) != start
+                pushfirst!(path, prev_position[first(path)])
+            end
+            return risk, path
+        end
+        if risk <= tentative_risk[position]
+            tentative_risk[position] = risk
+            for neighbor in [position + move for move in moves if position + move in cavern]
+                neighbor_risk = risk + chitons[neighbor]
+                if neighbor_risk < tentative_risk[neighbor]
+                    tentative_risk[neighbor] = neighbor_risk
+                    prev_position[neighbor] = position
+                    push!(queue, (neighbor, neighbor_risk))
+                end
             end
         end
     end
     nothing  # finish is not reachable
 end
 
-function expand_cave(risks)
-    risks = [
-        risks    risks.+1 risks.+2 risks.+3 risks.+4
-        risks.+1 risks.+2 risks.+3 risks.+4 risks.+5
-        risks.+2 risks.+3 risks.+4 risks.+5 risks.+6
-        risks.+3 risks.+4 risks.+5 risks.+6 risks.+7
-        risks.+4 risks.+5 risks.+6 risks.+7 risks.+8
+function expand_chitons(chitons)
+    chitons = [
+        chitons    chitons.+1 chitons.+2 chitons.+3 chitons.+4
+        chitons.+1 chitons.+2 chitons.+3 chitons.+4 chitons.+5
+        chitons.+2 chitons.+3 chitons.+4 chitons.+5 chitons.+6
+        chitons.+3 chitons.+4 chitons.+5 chitons.+6 chitons.+7
+        chitons.+4 chitons.+5 chitons.+6 chitons.+7 chitons.+8
     ]
-    risks .= mod1.(risks, 9)
-    risks
+    chitons .= mod1.(chitons, 9)
+    chitons
 end
 
 chitons = collect.(readlines("day15/chitons.txt"))
 chitons = parse.(Int, permutedims(hcat(chitons...)))
 
-p1 = find_lowest_total_risk(chitons)
+p1, _ = find_low_risk_path(chitons)
 
 @show p1
 
-p2 = find_lowest_total_risk(expand_cave(chitons))
+p2, _ = find_low_risk_path(expand_chitons(chitons))
 
 @show p2
