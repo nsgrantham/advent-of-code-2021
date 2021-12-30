77d80f2714105d7ac9c470b63d2b852809ea07ad
Neal Grantham <neal@nsgrantham.com>
2021-12-29 17:10:47 -0800

    Small refactors

--- day25/solve.jl
+++ day25/solve.jl
@@ -1,49 +1,56 @@
-struct Cucumbers
-    east::Dict{Int, Vector{Int}}
-    south::Dict{Int, Vector{Int}}
-    dims::Tuple{Int, Int}
+struct SeaCucumberHerd
+    eastward::Dict{Int, Vector{Int}}
+    southward::Dict{Int, Vector{Int}}
+    xmax::Int
+    ymax::Int
 end
 
-function step!(cucumbers::Cucumbers)
+function moveeast!(herd::SeaCucumberHerd)
     n = 0
-    xmax, ymax = cucumbers.dims
-    east = deepcopy(cucumbers.east)
-    for (x, ys) in east
+    for (x, ys) in deepcopy(herd.eastward)
         for (i, y) in enumerate(ys)
-            ynew = mod1(y + 1, ymax)
+            ynew = mod1(y + 1, herd.ymax)
             ynew in ys && continue
-            ynew in keys(cucumbers.south) && x in cucumbers.south[ynew] && continue
-            cucumbers.east[x][i] = ynew
-            n += 1
-        end
-    end
-    south = deepcopy(cucumbers.south)
-    for (y, xs) in south
-        for (i, x) in enumerate(xs)
-            xnew = mod1(x + 1, xmax)
-            xnew in xs && continue
-            xnew in keys(cucumbers.east) && y in cucumbers.east[xnew] && continue
-            cucumbers.south[y][i] = xnew
+            ynew in keys(herd.southward) && x in herd.southward[ynew] && continue
+            herd.eastward[x][i] = ynew
             n += 1
         end
     end
     n
 end
 
-function Base.parse(::Type{Cucumbers}, lines::Vector{String})
-    east = Dict{Int, Vector{Int}}()
-    south = Dict{Int, Vector{Int}}()
-    for (x, line) in enumerate(lines)
-        for (y, char) in enumerate(collect(line))
-            char == '>' && push!(get!(valtype(east), east, x), y)
-            char == 'v' && push!(get!(valtype(south), south, y), x)
+function movesouth!(herd::SeaCucumberHerd)
+    n = 0
+    for (y, xs) in deepcopy(herd.southward)
+        for (i, x) in enumerate(xs)
+            xnew = mod1(x + 1, herd.xmax)
+            xnew in xs && continue
+            xnew in keys(herd.eastward) && y in herd.eastward[xnew] && continue
+            herd.southward[y][i] = xnew
+            n += 1
         end
     end
-    Cucumbers(east, south, (length(lines), length(lines[1])))
+    n
 end
 
-cucumbers = parse(Cucumbers, readlines("day25/cucumbers.txt"))
+function step!(herd::SeaCucumberHerd)
+    moveeast!(herd) + movesouth!(herd)
+end
 
-p1 = maximum(Iterators.takewhile(_ -> step!(cucumbers) > 0, Iterators.countfrom(1))) + 1
+function Base.parse(::Type{SeaCucumberHerd}, lines::Vector{String})
+    eastward = Dict{Int, Vector{Int}}()
+    southward = Dict{Int, Vector{Int}}()
+    for (x, line) in enumerate(lines)
+        for (y, char) in enumerate(collect(line))
+            char == '>' && push!(get!(valtype(eastward), eastward, x), y)
+            char == 'v' && push!(get!(valtype(southward), southward, y), x)
+        end
+    end
+    SeaCucumberHerd(eastward, southward, length(lines), length(lines[1]))
+end
+
+herd = parse(SeaCucumberHerd, readlines("day25/cucumbers.txt"))
+
+@time p1 = maximum(Iterators.takewhile(_ -> step!(herd) > 0, Iterators.countfrom(1))) + 1
 
 @show p1
